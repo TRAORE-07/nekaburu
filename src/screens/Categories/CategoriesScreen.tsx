aeb54a388824761c169a93c4d8d5ecb8a3064f37
TRAORE-07 <traoredjeneba0711@gmail.com>
2025-07-04 15:33:39 +0530

    nekaburu

--- src/screens/Categories/CategoriesScreen.tsx
+++ src/screens/Categories/CategoriesScreen.tsx
@@ -1,12 +1,18 @@
-// src/screens/BuyAgain/BuyAgainScreen.tsx
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+// CATEGORIES SCREEN
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, ScrollView, Dimensions, Image, TouchableOpacity, Animated } from 'react-native';
+import CustomHeader from '../../components/layout/CustomHeader';
+import Ionicons from '@expo/vector-icons/Ionicons'; // Pour les icônes
 
 function CategoriesScreen(): React.JSX.Element {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Écran "Categories"</Text>
       {/* Ajoutez ici le contenu de votre page "Categories" */}
+
+
+
+
     </View>
   );
 }
